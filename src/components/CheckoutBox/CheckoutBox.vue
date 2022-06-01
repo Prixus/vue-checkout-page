e6ebfad33a5d2824ac4bb65f9239640c25c2d3d7
prixus <simonpetercalamno@gmail.com>
2022-06-01 17:56:10 +0800

    Integrated bootstrap vue and vue cli

---
+++ src/components/CheckoutBox/CheckoutBox.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="">
+            <img
+                    alt="product"
+                    src="https://mrdevelopertestassets.s3.ap-southeast-2.amazonaws.com/classic-tee.jpg"
+            >
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CheckoutBox'
+    }
+</script>
+
+<style scoped>
+
+</style>
